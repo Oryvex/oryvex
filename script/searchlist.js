84bfe6227fda8fa45c671e094e1304fe5bb2bd00
Oryvex <130756455+Oryvex@users.noreply.github.com>
2023-05-13 01:06:50 +0530

    Update searchlist.js

--- script/searchlist.js
+++ script/searchlist.js
@@ -71,7 +71,6 @@ const searchlist = {
   et: {
     desc: "Entertainment",
     items: {
-      Hotstar: "https://www.hotstar.com/in/explore?search_query=",
       PrimeVideo: "https://www.primevideo.com/search/ref=atv_nb_sug?ie=UTF8&phrase=",
       YouTube: "https://www.youtube.com/results?search_query=",
       AppleTV: "https://tv.apple.com/search?term=",
